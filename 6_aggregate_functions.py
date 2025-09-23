2648967be75330ca7dbe88b79ea75a637a2cc875
luis-kael <sithukaungsett2001@gmail.com>
2025-09-24 02:32:14 +0630

    day4

---
+++ 6_aggregate_functions.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+array = np.array ([[1, 2, 3, 4, 5],
+                   [6, 7, 8, 9, 10]])
+
+# print(np.sum(array))
+# print(np.mean(array)) Average
+# print(np.std(array))
+# print(np.var(array))
+# print(np.min(array))
+# print(np.max(array))
+# print(np.argmin(array)) # lowest index number
+# print(np.argmax(array)) # highest index number
+
+# print(np.sum(array, axis= 0)) sum column
+# print(np.sum(array, axis= 1)) sum row
