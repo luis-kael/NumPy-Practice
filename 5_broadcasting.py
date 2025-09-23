3b12b2cc3becc9ee279b5634053da32918d002e3
luis-kael <sithukaungsett2001@gmail.com>
2025-09-23 17:20:32 +0630

    day3

---
+++ 5_broadcasting.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+array1 = np.array([[1, 2 , 3, 4],
+                   [5, 6, 7, 8],
+                   [9, 10, 11, 12],
+                   [13, 14, 15, 16]])
+array2 = np.array([[1], [2], [3], [4]])
+
+print(array1.shape)
+print(array2.shape)
+
+print(array1 * array2)
